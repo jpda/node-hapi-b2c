cf8d1b3d6842c926c83950a0477485cf84528447
jpda <john.dandison@live.com>
2019-10-04 13:35:40 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,103 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const uuid4 = require('uuid/v4');
+const { Issuer, generators } = require('openid-client');
+const scheme = require('./scheme');
+
+const oidcConfig = {
+    callbackUrl: "<redirect_uri>",
+    clientId: "<client_id>",
+    responseTypes: ["id_token"],
+    scopes: "openid profile",
+    discoveryUrl: "<b2c policy discovery uri>"
+};
+const cookieName = "oidc";
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    // configure oidc provider & infra
+    const issuer = await Issuer.discover(oidcConfig.discoveryUrl);
+    console.log('Discovered issuer %s %O', issuer.issuer, issuer.metadata);
+
+    const client = new issuer.Client({
+        client_id: oidcConfig.clientId,
+        redirect_uri: oidcConfig.callbackUrl,
+        response_types: oidcConfig.responseTypes,
+    });
+
+    // configure hapi authentication scheme
+    server.auth.scheme('oidc', scheme({
+        cookieName, callbackUrl: oidcConfig.callbackUrl, scope: oidcConfig.scopes, client
+    }));
+
+    // configure hapi auth strategy
+    server.auth.strategy('oidc', 'oidc', {
+        password: uuid4(),
+    });
+
+    // sets oidc strategy as default
+    server.auth.default("oidc");
+
+    // protected endpoint, noted by absence of 'options:auth' object
+    server.route([{
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            if (request.auth.isAuthenticated) {
+                return "Hello " + JSON.stringify(request.auth.artifacts);
+            }
+            return h.redirect("/signin");
+        }
+    },
+    // unprotected endpoint for login, note 'options:auth:false'
+    {
+        method: "GET",
+        path: "/signin",
+        handler: (request, h) => {
+
+            return "<div>sign in!</div>";
+        },
+        options: {
+            auth: false
+        }
+    },
+    // callback from oidc provider. parses cookie data for state/nonce to ensure they match, sets auth data in hapi pipeline and redirects home
+    {
+        method: 'POST',
+        path: "/signin",
+        handler: async (request, h) => {
+            try {
+                const state = request.state.oidc.state;
+                const nonce = request.state.oidc.nonce;
+                h.request.raw.req.body = request.payload; // input object to callbackParams needs to match http.incomingMessage signature
+                const params = client.callbackParams(h.request.raw.req);
+                const tokenSet = await client.callback(oidcConfig.callbackUrl, params, { state, nonce });
+                console.log('received and validated tokens %j', tokenSet);
+                console.log('validated ID Token claims %j', tokenSet.claims());
+                h.state(cookieName, { credentials: tokenSet, artifacts: tokenSet.claims() });
+                return h.redirect("/");
+            } catch (err) {
+                request.log(['error', 'auth'], err.error_description);
+                throw err;
+            }
+        },
+        options: {
+            auth: false
+        }
+    }
+    ]);
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+init();
---
+++ src/oidc.js
@@ -0,0 +1,70 @@
+// modified from https://github.com/JbIPS/hapi-oidc
+// currently unused
+
+const Url = require('url');
+
+const { Issuer, generators } = require('openid-client');
+const Hoek = require('@hapi/hoek');
+
+const pkg = require('../package.json');
+const scheme = require('./scheme');
+
+const MANUAL_SETTINGS = ['issuer', 'authorization', 'token', 'userinfo', 'jwks'];
+const PLUGIN_DEFAULTS = {
+  cookie: 'oidc',
+  scope: 'openid',
+  useGraphForUserInfo: false
+};
+
+exports.plugin = {
+  pkg,
+  register: async (server, options) => {
+    const config = Hoek.applyToDefaults(PLUGIN_DEFAULTS, options);
+
+    // Validate config
+    if (!config.clientId || !config.clientSecret) throw new Error('You must provide a clientId and a clientSecret');
+    if (!config.callbackUrl) throw new Error('You must provide a callbackUrl');
+    if (!config.discoverUrl && MANUAL_SETTINGS.some(k => !(k in config))) throw new Error('You must provide a discoverUrl or a valid manual settings');
+
+    const cookieName = config.cookie;
+    const issuer = config.discoverUrl ? await Issuer.discover(config.discoverUrl) : new Issuer({
+      issuer: config.issuer,
+      authorization_endpoint: config.authorization,
+      token_endpoint: config.token,
+      userinfo_endpoint: config.userinfo,
+      jwks_uri: config.jwks,
+    });
+    const client = new issuer.Client({
+      client_id: config.clientId,
+      client_secret: config.clientSecret,
+    });
+
+    server.route({
+      method: 'GET',
+      path: Url.parse(config.callbackUrl).path,
+
+      handler: async (request, h) => {
+        const { state } = request.state.oidc;
+        try {
+          // use graph for userinfo for b2c? or skip
+          if (config.useGraphForUserInfo) {
+            const token = await client.authorizationCallback(
+              config.callbackUrl, request.query, { state },
+            );
+            const userInfos = await client.userinfo(token);
+            h.state(cookieName, { credentials: token, artifacts: userInfos });
+          } else {
+            h.state(cookieName, { credentials: "", artifacts: [] });
+          }
+          return h.redirect(state).takeover();
+        } catch (err) {
+          request.log(['error', 'auth'], err.error_description);
+          throw err;
+        }
+      },
+    });
+    server.auth.scheme('oidc', scheme({
+      cookieName, callbackUrl: config.callbackUrl, scope: config.scope, client,
+    }));
+  },
+};
